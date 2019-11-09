f423be1f6e0b1b2eb83e1d67137b3ea0a45c1008
katerinamakri <kathrin023@gmail.com>
2019-11-09 17:33:10 +0200

    Fixes in UI - Working on responsiveness

--- src/Search.js
+++ src/Search.js
@@ -5,14 +5,36 @@ class Search extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="search-content">
 				<form method="get" className="form">
 					<div className="input-container">
-						<i className="fas fa-search position-left"></i>
+						{/*<i className="fas fa-search position-left"></i>*/}
 						<input type="text" className="search"/>
-						<i className="fas fa-microphone position-right"></i>
+						{/*<i className="fas fa-microphone position-right"></i>*/}
 					</div>
-					<div>
+					<div className="list-container">
+						<ul>
+							<li>Glyfada Golf Club of Athens, Glyfada,Athens area,Athens Coast,Palaio Faliro</li>
+							<li>National Archaeological Museum of Athens, Athens area,Palaio Faliro,Acropolis,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Glyfada Golf Club of Athens, Glyfada,Athens area,Athens Coast,Palaio Faliro</li>
+							<li>National Archaeological Museum of Athens, Athens area,Palaio Faliro,Acropolis,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Glyfada Golf Club of Athens, Glyfada,Athens area,Athens Coast,Palaio Faliro</li>
+							<li>National Archaeological Museum of Athens, Athens area,Palaio Faliro,Acropolis,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>
+							<li>Athens Concert Hall (Megaron Moussikis), Kaisariani,Zografos,Athens area,Palaio Faliro,Athens, Greece</li>											
+						</ul>
+					</div>
+					<div className="button-container">
 						<button>Click to Search</button>
 					</div>
 				</form>
